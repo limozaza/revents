f872c51e6d121182c6446b57e82ad52d6d243e6e
limozaza <zakboite@gmail.com>
2018-06-03 14:43:53 +0200

    adding and stylin nav igation

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+
+import {Container} from 'semantic-ui-react';
 
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Re-vents</h1>
-        <EventDashboard/>
+      <div>
+        <NavBar/>
+        <Container className='main'>
+          <EventDashboard/>
+        </Container>
       </div>
+      
     );
   }
 }
